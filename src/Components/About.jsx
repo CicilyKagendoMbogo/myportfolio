b3d613b6a16820ac6895866387b40be3dbf4c59e
Victor Ndiritu <87909835+VickyTaar@users.noreply.github.com>
2023-10-23 04:42:19 +0300

    Update About.jsx test

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -31,7 +31,7 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "Web designer",
   "User experience",
   "Inclusive design",
   "Focus group testing",
